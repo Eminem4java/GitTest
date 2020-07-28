34ae0a4063a4b7882f5f519cfbeddc5aa6a2e75b
psb <LZDeverafter1122@163.com>
2020-07-28 12:07:52 +0800

    首次提交到git

---
+++ GitTest001/src/main/java/com/atguigu/git/GitHello.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class GitHello {
+    public static void main(String[] args) {
+        System.out.println("test test test!!!");
+    }
+}
---
+++ GitTest001/src/test/java/com/atguigu/git/MyTest.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("我是测试类!");
+    }
+}
