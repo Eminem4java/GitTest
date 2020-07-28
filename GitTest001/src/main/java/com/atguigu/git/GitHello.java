0245bd07f5730a7e61b00124f3b5d5d0801f1d62
psb <LZDeverafter1122@163.com>
2020-07-28 17:35:50 +0800

    测试SSH

--- GitTest001/src/main/java/com/atguigu/git/GitHello.java
+++ GitTest001/src/main/java/com/atguigu/git/GitHello.java
@@ -10,5 +10,6 @@ public class GitHello {
         System.out.println("再次在线更新!");
         System.out.println("先去拉取GitHub远程仓库进行更新后,在进行新的代码提交测试!");
         System.out.println("项目组成员A通过clone项目,参与到项目开发,他的任务已完成,提交代码,O(∩_∩)O哈哈~");
+        System.out.println("测试SSH");
     }
 }
