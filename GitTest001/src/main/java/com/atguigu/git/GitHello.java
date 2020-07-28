fac6b92790eea9e22204dd9788f314b972ecf8a5
psb <LZDeverafter1122@163.com>
2020-07-28 12:16:21 +0800

    首次提交到git

--- GitTest001/src/main/java/com/atguigu/git/GitHello.java
+++ GitTest001/src/main/java/com/atguigu/git/GitHello.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitHello {
     public static void main(String[] args) {
         System.out.println("test test test!!!");
+        System.out.println("我就是要搞事!!!");
     }
 }
