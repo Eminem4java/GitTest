55729d0684e20c295f97ac5cefaaeb4525b30549
psb <LZDeverafter1122@163.com>
2020-07-28 14:49:05 +0800

    先去拉取GitHub远程仓库进行更新后,在进行新的代码提交测试!

--- GitTest001/src/main/java/com/atguigu/git/GitHello.java
+++ GitTest001/src/main/java/com/atguigu/git/GitHello.java
@@ -8,5 +8,6 @@ public class GitHello {
         System.out.println("创建dev分支!!!");
         System.out.println("在线更新!");
         System.out.println("再次在线更新!");
+        System.out.println("先去拉取GitHub远程仓库进行更新后,在进行新的代码提交测试!");
     }
 }
